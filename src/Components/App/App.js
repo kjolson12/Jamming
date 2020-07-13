e4eef9c7083ff8f6e9d7097fd70f1ee7327b394e
kjolson12 <kobaseball02@gmail.com>
2020-07-12 19:50:42 -0700

    Jamming addTrack

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -27,8 +27,38 @@ class App extends React.Component {
         artist: 'Travis Scott',
         album: 'Rodeo',
         id: 3
+      }],
+      playlistName: 'When the preworkout hit',
+      playlistTracks: [{
+        name: 'Californication',
+        artist: 'Red Hot Chili Peppers',
+        album: 'Californication',
+        id: 4
+      },
+      {
+        name: 'Cant Stop',
+        artist: 'Red Hot Chili Peppers',
+        album: 'Single',
+        id: 5
+      },
+      {
+        name: 'Under the Bridge',
+        artist: 'Red Hot Chili Peppers',
+        album: 'Blood Sugar Sex Majik',
+        id: 6
       }]
+    };
+
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if(tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
     }
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
   }
 
   render() {
@@ -38,8 +68,10 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
-            <Playlist />
+            <SearchResults searchResults={this.state.searchResults}
+            onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName}
+            playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
