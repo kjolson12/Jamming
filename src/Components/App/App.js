60b25524f508b7f7f9be6493e8752214914846e6
kjolson12 <kobaseball02@gmail.com>
2020-07-13 20:21:27 -0700

    Jamming updatePlaylistName

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -51,6 +51,7 @@ class App extends React.Component {
 
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -68,6 +69,10 @@ class App extends React.Component {
     this.setState({playlistTracks: tracks});
   }
 
+  updatePlaylistName(name) {
+    this.setState({playlistName: name});
+  }
+
   render() {
     return (
       <div>
@@ -79,7 +84,8 @@ class App extends React.Component {
             onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName}
             playlistTracks={this.state.playlistTracks}
-            onRemove={this.removeTrack} />
+            onRemove={this.removeTrack}
+            onNameChange={this.updatePlaylistName} />
           </div>
         </div>
       </div>
